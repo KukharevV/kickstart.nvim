cd110a718d90118d9a745fcbb51fc74b8e6f3b1d
Vladyslav Kukharev <v.kukharev98@gmail.com>
2025-02-04 18:55:02 +0400

    add vim-tmux-navigator

---
+++ lua/custom/keymaps/init.lua
@@ -0,0 +1,9 @@
+-- Toggle between dark and light theme
+vim.keymap.set('n', '<leader>tt', function()
+  if vim.o.background == 'dark' then
+    vim.o.background = 'light'
+  else
+    vim.o.background = 'dark'
+  end
+  print('Switched to ' .. vim.o.background .. ' mode')
+end, { desc = 'Toggle between dark and light mode' })
