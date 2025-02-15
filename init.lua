9dfb78db771920ee340344644175b2ecd7e016cb
Vladyslav Kukharev <v.kukharev98@gmail.com>
2025-02-16 03:35:18 +0400

    add neorg

--- init.lua
+++ init.lua
@@ -125,6 +125,8 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
+vim.o.conceallevel = 2
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
